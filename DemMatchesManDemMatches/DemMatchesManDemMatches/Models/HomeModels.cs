cae57b304487028985031929f6aaca76c26f8929
anaheino <anaheino@gmail.com>
2016-04-12 11:25:18 +0300

    priv-public
    priv-publ

--- DemMatchesManDemMatches/DemMatchesManDemMatches/Models/HomeModels.cs
+++ DemMatchesManDemMatches/DemMatchesManDemMatches/Models/HomeModels.cs
@@ -104,7 +104,7 @@ namespace DemMatchesManDemMatches.Models
             public String Round { get; set; }
             public int RoundNumber { get; set; }
             public DateTime MatchDate { get; set; }
-            private string HomeTeam { get; set; }
+            public string HomeTeam { get; set; }
             public string AwayTeam { get; set; }
             public int HomeGoals { get; set; }
             public int AwayGoals { get; set; }
